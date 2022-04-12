a7ce31dbbb95d29e2deb0ae458082f5ed5f46545
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-04-12 18:45:58 -0400

    added favorited field to questionanswerserializer

--- questionbox/serializers.py
+++ questionbox/serializers.py
@@ -98,5 +98,6 @@ class QuestionAnswerSerializer(serializers.ModelSerializer):
             "question",
             "created",
             "user",
+            "favorited",
             "answers",
         )
